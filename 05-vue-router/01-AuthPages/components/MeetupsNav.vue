1923d7edbaa159d419b945cea6daded600d06506
Oleg Chabak <chabak_ol@magnit.ru>
2023-04-11 22:24:49 +0300

    Решение ДЗ 05-vue-router/01-AuthPages

--- 05-vue-router/01-AuthPages/components/MeetupsNav.vue
+++ 05-vue-router/01-AuthPages/components/MeetupsNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="nav">
-    <a href="/login" class="nav__link">Вход</a>
-    <a href="/register" class="nav__link">Регистрация</a>
+    <router-link :to="{ name: 'PageLogin' }" class="nav__link">Вход</router-link>
+    <router-link :to="{ name: 'PageRegister' }" class="nav__link">Регистрация</router-link>
   </nav>
 </template>
 
