c461128618513f13e70195c66619f9ba0732f1e4
Oleg Chabak <chabak_ol@magnit.ru>
2023-04-27 15:20:41 +0300

    Решение ДЗ 06-wrappers/01-UiLink

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -24,7 +24,7 @@ export default {
   }),
 
   unmounted() {
-    this.toasts.forEach((toast) => clearTimeout(toast.id));
+    Object.keys(this.toasts).forEach((toast) => clearTimeout(+toast));
   },
 
   methods: {
